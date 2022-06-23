65591b552952c91b93b5698ed291934303a2d295
SGSANJAY <sgsanjay044@gmail.com>
2022-06-23 23:24:20 +0530

    first update

---
+++ Hand_cricket/Hand_cricket.js
@@ -0,0 +1,16 @@
+/*var con=document.querySelector(".b")
+var p=document.querySelector("#p")
+var cpp=document.querySelector("#cp")
+var cp
+con.addEventListener('click',function(){
+    con.textContent="Even wins"
+})
+function randam(){
+    cp=(Math.random()*5)+1
+    switch(cp){
+        case 1:
+            cpp.img.src="one.jpeg"
+
+    }
+}*/
+document.getElementById("#cp").src="zero.jpeg"
